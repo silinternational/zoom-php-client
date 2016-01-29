2134da179dd1aa17f921d966a5edc06f5dce4404
Matt Henderson <matt_henderson@sil.org>
2016-01-29 10:18:17 -0500

    Send data to Zoom as POST form fields (not JSON).

--- src/descriptions/user.php
+++ src/descriptions/user.php
@@ -15,112 +15,112 @@
                 'api_key' => [
                     'required' => true,
                     'type' => 'string',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'api_secret' => [
                     'required' => true,
                     'type' => 'string',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'email' => [
                     'required' => true,
                     'type' => 'string',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'type' => [
                     'required' => false,
                     'type' => 'integer',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'password' => [
                     'required' => true,
                     'type' => 'string',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'first_name' => [
                     'required' => false,
                     'type' => 'string',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'last_name' => [
                     'required' => false,
                     'type' => 'string',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'disable_chat' => [
                     'required' => false,
                     'type' => 'boolean',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'enable_e2e_encryption' => [
                     'required' => false,
                     'type' => 'boolean',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'enable_silent_mode' => [
                     'required' => false,
                     'type' => 'boolean',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'disable_group_hd' => [
                     'required' => false,
                     'type' => 'boolean',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'disable_recording' => [
                     'required' => false,
                     'type' => 'boolean',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'enable_cmr' => [
                     'required' => false,
                     'type' => 'boolean',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'enable_auto_recording' => [
                     'required' => false,
                     'type' => 'boolean',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'enable_cloud_auto_recording' => [
                     'required' => false,
                     'type' => 'boolean',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'enable_webinar' => [
                     'required' => false,
                     'type' => 'boolean',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'webinar_capacity' => [
                     'required' => false,
                     'type' => 'integer',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'enable_large' => [
                     'required' => false,
                     'type' => 'boolean',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'large_capacity' => [
                     'required' => false,
                     'type' => 'integer',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'disable_feedback' => [
                     'required' => false,
                     'type' => 'boolean',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'disable_jbh_reminder' => [
                     'required' => false,
                     'type' => 'boolean',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'dept' => [
                     'required' => false,
                     'type' => 'string',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
             ]
         ],
@@ -137,7 +137,7 @@
                 'id' => [
                     'required' => true,
                     'type' => 'string',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
             ]
         ],
@@ -154,12 +154,12 @@
                 'email' => [
                     'required' => true,
                     'type' => 'string',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
                 'login_type' => [
                     'required' => true,
                     'type' => 'integer',
-                    'location' => 'json',
+                    'location' => 'postField',
                 ],
             ]
         ],
